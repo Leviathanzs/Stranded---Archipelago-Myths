ba7b973b94c36c57761b4307e456aaaf48b6e30d
Oky Satria W <okywidhiansyah@gmail.com>
2024-04-03 12:04:15 +0700

    Stat Tooltip

--- Assets/Scripts/Inventory & Item/StatDisplay.cs
+++ Assets/Scripts/Inventory & Item/StatDisplay.cs
@@ -2,16 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class StatDisplay : MonoBehaviour
+public class StatDisplay : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    public TextMeshProUGUI NameText;
-    public TextMeshProUGUI ValueText;
+    private PlayerBaseStats _stat;
+    public PlayerBaseStats Stat {
+        get { return _stat;} 
+        set
+        {
+            _stat = value;
+            UpdateStatValue();
+        }
+    }
+
+    private string _name;
+    public string Name {
+        get { return _name;} 
+        set {
+            _name = value;
+            nameText.text = _name.ToLower();
+        }
+    }
+
+    [SerializeField] TextMeshProUGUI nameText;
+    [SerializeField] TextMeshProUGUI valueText;
+    [SerializeField] StatTooltip tooltip;
 
     private void OnValidate()
     {
         TextMeshProUGUI[] texts = GetComponentsInChildren<TextMeshProUGUI>();
-        NameText = texts[0];
-        ValueText = texts[1];
+        nameText = texts[0];
+        valueText = texts[1];
+
+        if(tooltip == null)
+            tooltip = FindObjectOfType<StatTooltip>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        tooltip.ShowTooltip(Stat, Name);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        tooltip.HideTooltip();
+    }
+
+    public void UpdateStatValue()
+    {
+        valueText.text = _stat.Value.ToString();
     }
 }
