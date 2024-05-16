b899c725661003bba8f16b3bc7a9532dfb1a8171
Oky Satria W <okywidhiansyah@gmail.com>
2024-05-16 17:29:57 +0700

    Parallax Background
    Adding parallax script & update environment

--- Assets/Scripts/ParallaxBackground.cs
+++ Assets/Scripts/ParallaxBackground.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class ParallaxBackground : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Camera cam;
+    public Transform followTarget;
+    Vector2 startingPosition;
+
+    float startingZ;
+
+    Vector2 camMoveSinceStart => (Vector2)cam.transform.position - startingPosition;
+
+    float zDistanceFromTarget => transform.position.z - followTarget.position.z;
+
+    float clippingPlane => (cam.transform.position.z + (zDistanceFromTarget > 0 ? cam.farClipPlane : cam.nearClipPlane));
+
+    float parallaxFactor => Mathf.Abs(zDistanceFromTarget) / clippingPlane;
+
     void Start()
     {
-        
+        startingPosition = transform.position;
+        startingZ = transform.position.z;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void Update() 
     {
-        
+        Vector2 newPosition = startingPosition + camMoveSinceStart * parallaxFactor;
+
+        transform.position = new Vector3(newPosition.x, newPosition.y, startingZ);    
     }
 }
