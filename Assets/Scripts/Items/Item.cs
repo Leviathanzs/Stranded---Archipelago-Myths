87256d96f943f21c61a0d920ade7ad93b23fba36
Oky Satria W <okywidhiansyah@gmail.com>
2024-09-01 21:52:38 +0700

    Percent Item Tooltip Bug

--- Assets/Scripts/Items/Item.cs
+++ Assets/Scripts/Items/Item.cs
@@ -29,7 +29,7 @@ public class Item : ScriptableObject
 
     public virtual Item GetCopy()
     {
-        return this;   
+        return Instantiate(this);
     }
 
     public virtual void Destroy()
